d6f22ed927e70327b1ad81338b163b751926f6cd
juliafpeck <juliafpeck@gmail.com>
2024-11-20 15:48:05 -0800

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,10 +1,10 @@
 export const CONFIG = {
-  tries: 7, // This changes how many tries you get to finish the game
-  language: 'YourLanguageHere', // This changes the display name for your language
-  wordLength: 6, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this game!
+  tries: 6, // This changes how many tries you get to finish the game
+  language: 'Ladino', // This changes the display name for your language
+  wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
+  author: 'Julia Peck', // Put your name here so people know who made this game!
   authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
+  wordListSource: 'Perahya 2012', // Describe the source material for your words here
   wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
